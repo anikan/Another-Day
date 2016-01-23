8d854d248da77abb15bb943c843b2fd6b3d4ab8f
anikan <anikan33@gmail.com>
2016-01-23 14:16:43 -0800

    Added basic gaze system, tutorial in testScript.cs

---
+++ Assets/Scripts/ActivatableObject.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class ActivatableObject : MonoBehaviour {
+
+    //True while a tap is active and focused on it.
+    public bool isActive;
+
+    //True to prevent multiple activations in a single tap.
+    //Set this to true the instant it is active, and set it to false when the object is no longer active, ready for the next tap.
+    public bool triggered = false;
+    public int numTimes = 0;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Activate()
+    {
+        isActive = true;
+    }
+
+    public void Deactivate()
+    {
+        isActive = false;
+    }
+}
---
+++ Assets/Scripts/OverallStatus.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverallStatus : MonoBehaviour {
+
+    public static bool phoneCompleted;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
