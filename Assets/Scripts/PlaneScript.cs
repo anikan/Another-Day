e472811dfb76d98f5cb5ccdc422dcff4f2f82ade
connor-a-smith <c3smith@ucsd.edu>
2016-01-23 22:01:13 -0700

    Added ability to close windows, and text bubbles for darkening room
    1. Added a “PlaneScript” to the blinds on the window.
    2. When the blinds are activated, the lights are destroyed
    3. When blinds are activated, text bubble pops up
    4. When seen starts, text bubble “It’s so bright” pops up.

---
+++ Assets/Scripts/PlaneScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaneScript : ActivatableObject {
+
+    public GameObject raycastSource;
+
+    public GameObject directionalLight;
+    public GameObject pointLight;
+
+    public GameObject planeObject;
+
+    public string message= "I don't think \nthat's a \ngood idea";
+	// Use this for initialization
+	void Start () {
+
+		Vector3 playerPosition = raycastSource.transform.position;
+
+            float distance = Vector3.Magnitude((transform.position - playerPosition) * .8f);
+
+		GameObject bubble = (GameObject)GameObject.Instantiate(textBubblePrefab, playerPosition + distance * raycastSource.transform.forward, new Quaternion());
+      bubble.GetComponent<TextBubbleScript>().fullMessage = "It's \n so \n bright...";
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Trigger the effect if not previously triggered this activation.
+        if (isActive && !triggered)
+        {
+            triggered = true;
+            numTimes++;
+
+            Vector3 playerPosition = raycastSource.transform.position;
+
+            float distance = Vector3.Magnitude((transform.position - playerPosition) * .8f);
+
+            directionalLight.SetActive(false);
+            pointLight.SetActive(false);
+            planeObject.SetActive(false);
+
+                        
+            //Create the bubble halfway between the object and the player.
+            GameObject bubble = (GameObject)GameObject.Instantiate(textBubblePrefab, playerPosition + distance * raycastSource.transform.forward, new Quaternion());
+
+            bubble.GetComponent<TextBubbleScript>().fullMessage = message;
+
+
+
+        }
+
+        //Turn off the triggering
+        else if (!isActive && triggered)
+        {
+            triggered = false;
+        }
+    }
+}
