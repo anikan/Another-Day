eea2f368d1f7d0f9093596e5cb07092e8de68074
Kristin Agcaoili <kagcaoil@ucsd.edu>
2016-01-24 03:28:55 -0700

    not really cleaned stuff yet
    tried to clean it up

---
+++ Assets/Scripts/KnifeScript.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnifeScript : ActivatableObject {
+	
+	public GameObject raycastSource;
+	public string message= "...";
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Trigger the effect if not previously triggered this activation.
+		if (isActive && !triggered)
+		{
+			triggered = true;
+			numTimes++;
+			
+			Vector3 playerPosition = raycastSource.transform.position;
+			
+			float distance = Vector3.Magnitude((transform.position - playerPosition) * .8f);
+			
+			//Create the bubble halfway between the object and the player.
+			GameObject bubble = (GameObject)GameObject.Instantiate(textBubblePrefab, playerPosition + distance * raycastSource.transform.forward, new Quaternion());
+			
+			bubble.GetComponent<TextBubbleScript>().fullMessage = message;
+		}
+		
+		//Turn off the triggering
+		else if (!isActive && triggered)
+		{
+			triggered = false;
+		}
+	}
+}
