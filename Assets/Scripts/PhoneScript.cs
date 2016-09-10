cdaf59cc73d1ccf37c0549b9b2921e4c67aafd6a
anikan <anikan33@gmail.com>
2016-09-09 23:16:29 -0700

    Working on phone

--- Assets/Scripts/PhoneScript.cs
+++ Assets/Scripts/PhoneScript.cs
@@ -1,19 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 
 public class PhoneScript : ActivatableObject
 {
-    public List<MessageScript> messages;
+    //The content where scroll view objects go.
+    public GameObject contentCanvas;
+
     public GameObject ownMessagePrefab;
     public GameObject otherMessagePrefab;
 
     public Vector3 messageStartLocation;
 
+    float bubbleOffset = 20;
+
     // Use this for initialization
     void Start()
     {
-
     }
 
     // Update is called once per frame
@@ -24,18 +28,50 @@ public class PhoneScript : ActivatableObject
         {
             triggered = true;
         }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            SendMessage(true, "Oh Sad soul. Bio is slightly better imo\nw\nefwe");
+        }
     }
 
     void SendMessage(bool isOwn, string message)
     {
         if (isOwn)
         {
-           // GameObject.Instantiate(ownMessagePrefab, this.transform, false, );
+            GameObject messageObject = (GameObject) GameObject.Instantiate(ownMessagePrefab, this.transform, false);
+            messageObject.transform.parent = phoneCanvas.transform;
+
+
+            //First get the size of the text.
+            Text text = messageObject.GetComponentInChildren<Text>();
+            TextGenerator textGen = text.cachedTextGenerator;
+
+            float textHeight = textGen.GetPreferredHeight(message, text.GetGenerationSettings(text.rectTransform.rect.size));
+
+            //Then update the image and the text itself.
+            //Image
+            RectTransform messageRect = messageObject.GetComponent<RectTransform>();
+
+            Vector2 newRectSize = messageRect.sizeDelta + new Vector2(0, textHeight + bubbleOffset);
+
+            messageRect.sizeDelta = new Vector2(newRectSize.x, newRectSize.y);
+
+            //Text
+            newRectSize = text.rectTransform.sizeDelta + new Vector2(0, textHeight + bubbleOffset);
+
+            text.rectTransform.sizeDelta = new Vector2(newRectSize.x, newRectSize.y);
+
+            text.text = message;
         }
 
         else
         {
            // GameObject.Instantiate(otherMessagePrefab, this.transform, false, )
+
+            //Vibrate
+            //Scroll down.
+            //Play sound.
         }
     }
 }
--- Assets/Scripts/PhoneScrollScript.cs
+++ Assets/Scripts/PhoneScrollScript.cs
@@ -1,15 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+
 
 public class PhoneScrollScript : MonoBehaviour {
 
+    ScrollRect rect;
+
 	// Use this for initialization
 	void Start () {
-	
+        rect = GetComponent<ScrollRect>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
-	}
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+        //    rect.
+
+           // rect.CalculateLayoutInputVertical();
+        }
+
+        else if (Input.GetKeyDown(KeyCode.Z))
+        {
+            rect.velocity = new Vector2(0f, 1000f);
+
+        }
+    }
 }
